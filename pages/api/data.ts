b60aebbf58263cd4bba669251d57cd5f1eb20b7b
shykeiichi <imdark200@gmail.com>
2023-02-04 17:33:48 +0100

    Initial Commit

--- pages/api/data.ts
+++ pages/api/data.ts
@@ -1,13 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { get_data } from '@/src/data'
 
-type Data = {
-  name: string
-}
-
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<any>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  let data = await get_data();
+  res.status(200).json(data)
 }
